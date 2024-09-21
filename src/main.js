44864f19306de6acf33ebeddfacc9bc739462c51
Nicola <nicjah@hotmail.com>
2024-09-21 20:06:22 +0100

    Set up scope with $APPLOCALDATA/*

--- src/main.js
+++ src/main.js
@@ -5,5 +5,15 @@ const k = kaplay({
     height: 720,
     letterbox: true,
     global: false, 
-    scale: 2,
+    scale: 2,  
 });
+
+k.loadSprite("kriby", "./kriby.png");
+k.loadSprite("obstacles", "./obstacles.png");
+k.loadSprite("background", "./background.png");
+k.loadSprite("clouds", "./clouds.png");
+
+k.loadSound("jump", "./jump.wav");
+k.loadSound("hurt", "./hurt.wav");
+k.loadSound("confirm", "./confirm.wav");
+
